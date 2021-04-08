ddcce5fcf2e0b338ae214942c799fab0a24d527b
Ahmed Anas <ahmed.a.tamim@gmail.com>
2021-04-08 17:08:00 +0300

    minimal repro

---
+++ src/gatling/scala/frontline/sample/BasicSimulation.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2011-2018 GatlingCorp (https://gatling.io)
+ *
+ * All rights reserved.
+ */
+
+package frontline.sample
+
+import io.gatling.core.Predef._
+import io.gatling.core.feeder.BatchableFeederBuilder
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+  private def requestsBody: String ="""
+                                      |{
+                                      |    "invocationSequenceNumber": 1,
+                                      |    "tenantIdentifier": ${PROVIDER.jsonStringify()},
+                                      |    "subscriberIdentifier": ${MSISDN.jsonStringify()},
+                                      |    "multipleUnitUsage": {
+                                      |        "requestedUnit": {
+                                      |            "serviceSpecificUnits": 0.1
+                                      |        },
+                                      |        "usedUnitContainer": {
+                                      |            "serviceSpecificUnits": 0.1
+                                      |        },
+                                      |        "ratingGroup": "voice"
+                                      |    }
+                                      |}
+                                      |""".stripMargin
+
+  val accountFeeder: BatchableFeederBuilder[String]#F = csv("data/accounts.csv").circular
+
+  val httpProtocol = http
+    .enableHttp2
+    .baseUrl(s"https://charge-engine-load-test.private.central-eks.aureacentral.com/nchf-convergedcharging/v3/")
+    .header("Content-Type", "application/json")
+    .http2PriorKnowledge(Map("charge-engine-load-test.private.central-eks.aureacentral.com" -> true))
+    .shareConnections
+
+
+  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
+    .feed(accountFeeder)
+    .exec(http("Init Request")
+      .post("/chargingData")
+      .body(StringBody(requestsBody))
+      .check(status.is(201))
+    )
+
+  setUp(scn.inject(
+    constantConcurrentUsers(1000) during(300.seconds)
+  ).protocols(httpProtocol)).maxDuration(315.seconds)
+}
